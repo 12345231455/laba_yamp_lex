d5dc26c9cad1593357f4c7102f6a1f13220d68e1
12345231455 <147683836+12345231455@users.noreply.github.com>
2024-12-24 19:30:15 +0300

    Delete laba1_yamp_2var directory

--- laba1_yamp_2var/laba1_yamp_2var.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <iostream>
-#include "LexicalAnalyzer.h"
-#include "Token.h"
-
-int main() 
-{
-    LexicalAnalyzer Analysis("input.txt", "output.txt", "error.txt");
-    Analysis.LexAnalize();
-}
